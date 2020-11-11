5f57ac1a0fc7bdd83613f49a1a5019b9db6e0fdf
Jami <sreekarj@amazon.com>
2020-11-11 15:32:53 -0800

    test

---
+++ src/main/java/scan/test/sample.java
@@ -0,0 +1,45 @@
+package scan.test;
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class sample {
+
+	public static void main(String[] args) throws IOException {
+	
+     CloseableHttpClient httpCl = HttpClients.createDefault();
+
+     try {
+
+         HttpGet request = new HttpGet("https://sample/get");
+
+         CloseableHttpResponse response = httpCl.execute(request);
+
+         try {
+
+        
+             HttpEntity entity = response.getEntity();
+             if (entity != null) {
+                
+                 String result = EntityUtils.toString(entity);
+                 System.out.println(result);
+             }
+
+         } finally {
+             response.close();
+         }
+     } finally {
+    	 httpCl.close();
+     }
+     
+     
+     
+	}
+
+}
